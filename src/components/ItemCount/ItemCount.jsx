fe81fe7f5f2894dfbf7cb2ae2e080d2b99cd9a40
Alejandro Marquez <Alejandromarquez.alba@gmail.com>
2024-02-25 10:59:05 -0300

    second

---
+++ src/components/ItemCount/ItemCount.jsx
@@ -0,0 +1,9 @@
+const count = () => {
+    return (
+        <div>
+            <h1>0</h1>
+            <button>Aumentar +</button>
+            <button>Restar -</button>
+        </div>
+    )    
+}
--- src/main.jsx
+++ src/main.jsx
@@ -6,5 +6,7 @@ import './index.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <h1>Titulo</h1>
+    
   </React.StrictMode>,
 )
