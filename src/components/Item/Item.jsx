5190b9de51b69fc5f4e047e139ff92e5bbcc812a
Alejandro Marquez <Alejandromarquez.alba@gmail.com>
2024-03-30 04:07:28 -0300

    11,12

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -1,14 +1,15 @@
 import { Link } from 'react-router-dom'
-
 const Item = ({ id, name, category, img, edad})=>{
+
     return(
         <div>
             <h2>Categoría: {category}</h2>
             <h2>Nombre: {name}</h2>
             <img src={img} style={{width:200, height:200}}/>
             <p>Edad: {edad}</p>
-            <Link to={`./item/${id}`}>Ver más</Link>
+            <Link to={`/item/${id}`}>Ver más</Link>
         </div>
+
     )
 }
 
