8ac55dc81787a4a7054a567e7cf94583e368780e
Alejandro Marquez <Alejandromarquez.alba@gmail.com>
2024-04-01 04:21:36 -0300

    sdk_agregado

---
+++ src/services/firebase/firebaseConfig.js
@@ -0,0 +1,16 @@
+
+import { initializeApp } from "firebase/app";
+import { getFireStore } from 'firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBivphofcS0XdSslSDsXJbs9sdxI3XmUUw",
+  authDomain: "reactbd-26957.firebaseapp.com",
+  projectId: "reactbd-26957",
+  storageBucket: "reactbd-26957.appspot.com",
+  messagingSenderId: "559157876123",
+  appId: "1:559157876123:web:3c4cd9533005fbe669bd3e"
+};
+
+
+const app = initializeApp(firebaseConfig);
+
