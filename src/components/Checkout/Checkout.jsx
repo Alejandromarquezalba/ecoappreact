7cbef5162e59c3dda3ef9fa7b47c52db500f336c
Alejandro Marquez <Alejandromarquez.alba@gmail.com>
2024-04-02 05:00:21 -0300

    checkout actualizacion

--- src/components/Checkout/Checkout.jsx
+++ src/components/Checkout/Checkout.jsx
@@ -1,11 +1,73 @@
+import { useContext, useState } from "react"
+import { CardContext } from '../../context/CardContext'
+import { getDocs, collection, query, where, documentId, QuerySnapshot, writeBatch } from 'firebase/firestore'
+
+
 const Checkout = ()=>{
 
-    const createOrder = ()=>{
-        const objetctOrder = {
-            buyer : {},
-            items: [],
-            total: 0
+    const [loading, setLoading] = useState(false)
+    const [orderId, setOrderId] = useState(null)
+    const [card, total, clearCart] = useContext(CardContext)
+
+    const createOrder = async (userData)=>{
+
+        try{
+            setLoading(true)
+            const objetctOrder = {
+                buyer : {
+                    name: 'pepe grillo',
+                    email: 'correo@correo.correo',
+                    phone: 123456789
+                },
+                items: card,
+                total: total
+            }
+            const batch = writeBatch(db)
+            const outOfStock = []
+            const ids = card.map(prod => prod.id)
+    
+            const productsCollection = query(collection(db, 'products'), where(documentId(), 'in', ids))
+            
+            const querySnapshot = await getDocs(productsCollection)
+            
+            const { docs } = querySnapshot
+            docs.forEach(doc => {
+                //datos de db
+                const data = doc.data
+                const stockdb = data.stock
+                //datos de carrito
+                const productAddedToCart = card.find(prod => prod.id == doc.id)
+                const prodQuantity = productAddedToCart.quantity
+                if(stockdb >= prodQuantity){
+                    batch.update(doc.ref, { stock: stockDb - prodQuantity})
+                } else {
+                    outOfStock.push({ id: doc.id, ...data})
+                }
+            })
+            if (outOfStock.length == 0){
+                batch.commit()
+                const orderCollection = collection(db, 'orders')
+                const { id } = await addDoc(orderCollection, objetctOrder)
+
+                clearCart()
+                setOrderId(id)
+                
+            } else {
+                console.log('Hay productos sin stock')
+            }
         }
+        catch (error){
+            return (<h1>La orden está en proceso. . .</h1>)
+        }
+        finally{
+            setLoading(false)
+        }
+    }
+    if(loading){
+        console.log('La orden está en proceso')
+    }
+    if(orderId){
+        return <h1>El id de su orden es: {orderId}</h1>
     }
     return(
         <div>
--- src/context/CardContext.jsx
+++ src/context/CardContext.jsx
@@ -1,11 +1,48 @@
+import { useState, createContext, Children } from "react";
 
-const CardContext = () => {
+export const CardContext = createContext()
+
+export const CardProvider = ({ children }) => {
+    const [card, setCard] = useState([])
+
+    const addItem = (productToAdd)=>{
+        if(!isInCard(productToAdd.id)){
+            setCard(prev => [...prev, productToAdd])
+        } else {
+            console.error('Producto ya agregado')
+        }
+    }
+
+    const isInCard = (id)=>{
+        return card.some(prod => prod.id == id)
+    }
+
+    const getTotalQuantity = ()=>{
+        let stack = 0
+        card.forEach(prev => {
+            stack += prod.quantity
+        })
+        return stack
+    }
+
+    const totalQuantity = getTotalQuantity()
+
+    const getTotal = ()=>{
+        let stack = 0
+
+        card.forEach(prev => {
+            stack += prod.quantity * prod.price
+        })
+    }
+
+    const clearCart = ()=>{
+        setCard([])
+    }
+    const total = getTotal()
     return (
-        <h1>patata</h1>
+        <CardProvider.Provider value={{card, addItem, totalQuantity, total, clearCart}}>
+            {children}
+        </CardProvider.Provider>
     )
 }
 
-
-
-
-export default CardContext;
