518e191d13055cbacfb75e21aee5102f9d6c7e2a
Alejandro Marquez <Alejandromarquez.alba@gmail.com>
2024-04-02 22:00:38 -0300

    env creado

--- src/services/firebase/firebaseConfig.js
+++ src/services/firebase/firebaseConfig.js
@@ -4,12 +4,12 @@ import { initializeApp } from "firebase/app";
 import { getFireStore } from 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBivphofcS0XdSslSDsXJbs9sdxI3XmUUw",
-  authDomain: "reactbd-26957.firebaseapp.com",
-  projectId: "reactbd-26957",
-  storageBucket: "reactbd-26957.appspot.com",
-  messagingSenderId: "559157876123",
-  appId: "1:559157876123:web:3c4cd9533005fbe669bd3e"
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
 };
 
 const app = initializeApp(firebaseConfig);
